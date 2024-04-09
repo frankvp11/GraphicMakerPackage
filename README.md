33f8b2e132562fd184d921fdfada3407d062409e
frankvp11 <frankvanpaassen3@gmail.com>
2024-04-09 11:35:52 -0400

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+
+This project creates simple svg graphics
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name='NiceGUI',
+    version='1.0',
+    author='Frank',
+    author_email='frankvanpaassen4@gmail.com',
+    url='',
+    packages=['nicegui'],
+)
