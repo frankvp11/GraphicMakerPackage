154fead71da458584c75bdff229f4032f91f7056
frankvp11 <frankvanpaassen3@gmail.com>
2024-04-09 12:39:30 -0400

    make it so that I can pip install things

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@
 from setuptools import setup
 
 setup(
-    name='NiceGUI',
+    name='GraphicMaker',
     version='1.0',
     author='Frank',
     author_email='frankvanpaassen4@gmail.com',
-    url='',
-    packages=['nicegui'],
+    url='https://github.com/frankvp11/GraphicMakerPackage/',
+    include_package_data= True,
+    packages=['GraphicMaker', 'GraphicMaker.Shapes', 'GraphicMaker.Shapes.AlphaMask', 'GraphicMaker.Shapes.Circle', 'GraphicMaker.Shapes.Clip', 'GraphicMaker.Shapes.Group', 'GraphicMaker.Shapes.Line', 'GraphicMaker.Shapes.NGon', 'GraphicMaker.Shapes.Oval', 'GraphicMaker.Shapes.Polygon', 'GraphicMaker.Shapes.Rectangle', 'GraphicMaker.Shapes.Text'],
+    package_data={'GraphicMaker': ['Shapes/AlphaMask/alphamask.js', 'Shapes/Circle/circle.js', 'Shapes/Clip/clip.js', 'Shapes/Group/group.js', 'Shapes/Line/line.js', 'Shapes/NGon/ngon.js', 'Shapes/Oval/oval.js', 'Shapes/Polygon/polygon.js', 'Shapes/Rectangle/rectangle.js', 'Shapes/Text/text.js', 'svg.js']},
 )
